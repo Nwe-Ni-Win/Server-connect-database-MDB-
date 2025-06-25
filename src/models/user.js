03456450857e3fdea4a3023c5590dd5bd29a44c4
Nwe Ni Win <poethinzar665.com@gmail.com>
2025-06-25 14:32:16 +0630

    mongoose middlewares and methods

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema, Types } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -37,6 +38,16 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordisMatch(async function (password) {
+  return await bcrypt.compare(password, this.password);
+});
 
 userSchema.plugin(mongooseAggregatePaginate);
 
