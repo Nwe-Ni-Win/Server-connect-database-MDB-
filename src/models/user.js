1dc4a0585846fcbc4cb2fe79d4bd395ad1264315
Nwe Ni Win <poethinzar665.com@gmail.com>
2025-06-25 14:59:09 +0630

    jwt

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema, Types } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -45,9 +46,31 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.methods.isPasswordisMatch(async function (password) {
+userSchema.methods.isPasswordisMatch = async function (password) {
   return await bcrypt.compare(password, this.password);
-});
+};
+
+userSchema.method.generateAccessToken = async function (params) {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESSTOKEN_SECRET_KEY,
+    { expiresIn: ACCESSTOKEN_EXP_TIME }
+  );
+};
+
+userSchema.method.generateRefreshToken = async function (params) {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET_KEY,
+    { expiresIn: REFRESH_TOKEN_EXP_TIME }
+  );
+};
 
 userSchema.plugin(mongooseAggregatePaginate);
 
